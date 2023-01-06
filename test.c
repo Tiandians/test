e35a488406ca8f9144f60ca6f7861a1c2011beab
Tiandians <zhubaolin228@gmail.com>
2023-01-07 07:52:58 +0800

    some thing

--- test.c
+++ test.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
+#include <string.h>
 int main(void)
 {
-  int i, j, k = 5, *p;
-    
-  p = &k;
-  i = ++(*p);
-  j = (*p)++;
- printf("%d %d %d", i, j, k) ;   /* 输出的数字之间有一个空格 */
+	char a[] = "abcd\\1233";
+
+	printf("%s %d %d", a, sizeof(a), strlen(a));
+	return 0;
 }
